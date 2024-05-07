bcac7073fcdf1d354244a0b301246c3a5f975875
starcidapride <cuongnvtse160875@gmail.com>
2024-05-08 04:01:02 +0700

    123

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ import { readFileSync, writeFileSync } from "fs"
 import { join } from "path"
 
 export const GET = async () => {
-    const data = readFileSync(join(process.cwd(), "join-code.txt"), "utf8")
+    const data = readFileSync(join("tmp", "join-code.txt"), "utf8")
     return Response.json({ joinCode: data })
 }
 
@@ -12,6 +12,6 @@ interface JoinCode {
 
 export const POST = async (request: Request) => {
     const { joinCode } = await request.json() as JoinCode
-    writeFileSync(join(process.cwd(), "join-code.txt"), joinCode, "utf8")
+    writeFileSync(join("tmp", "join-code.txt"), joinCode, "utf8")
     return Response.json({ message: "ok" })
 }
