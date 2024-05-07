d5a0e439b112dc7abdabaa29e9c2683f78471203
starcidapride <cuongnvtse160875@gmail.com>
2024-05-08 04:07:51 +0700

    123123

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,14 +1,12 @@
 import { existsSync, readFileSync, writeFileSync } from "fs"
 import { join } from "path"
+import { json } from "stream/consumers";
 
 export const GET = async () => {
     const filePath = join("tmp", "join-code.txt");
-    if (!existsSync(filePath)) {
-        writeFileSync(filePath, "");
-    }
 
-    const data = readFileSync(filePath, "utf8");
-    return Response.json({ joinCode: data });
+    const joinCode = existsSync(filePath) ?  readFileSync(filePath, "utf8") : ""
+    return Response.json({ joinCode })
 }
 
 interface JoinCode {
